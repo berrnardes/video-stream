7e4a6422ce89467ee279c778ed729b5976e8f114
berrnardes <bernardes.iury@gmail.com>
2022-10-01 23:37:44 -0300

    Implementação da api e iniciando o componente row

--- src/Requests.js
+++ src/Requests.js
@@ -1,7 +1,7 @@
 const API_KEY = '1dca08d41c08ea718f87383d773568e2';
 
 const requests = {
-  fetchTrending: `/trend/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
 };
 
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -2,12 +2,16 @@ import React from 'react';
 import '../components/HomeScreen.css';
 import Nav from '../components/Nav';
 import Banner from '../components/Banner';
+import Request from '../Requests';
+import Row from '../components/Row';
 
 function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav />
       <Banner />
+      <Row title="Netflix Originals" fetchUrl={Request.fetchNetflixOriginals} isLargeRow />
+      <Row title="Tops Da Semana" fetchUrl={Request.fetchTrending} isLargeRow />
     </div>
   );
 }
